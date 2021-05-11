e547c54f860c93def6621985b9ee2f701d55bc27
Aleksandra Sypula <aleksandra.sypula@gmail.com>
2021-05-11 20:08:03 +0200

    Warrior class added

--- Warriors/Warriors.h
+++ Warriors/Warriors.h
@@ -1,1 +1,25 @@
 #pragma once
+#include <vector>
+
+class Warrior
+{
+protected:
+	int health;
+	int defense;
+	int power;
+public:
+	int leftHealth() { return this->health; };
+
+};
+
+template <typename T>
+class Army
+{
+	std::vector<T> army;
+public:
+	void addWarrior(T newWarrior)
+	{
+		army.push_back(newWarrior);
+	}
+
+};
