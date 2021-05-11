d717391abb16f69915e6737f79959cf40dd4c27b
Aleksandra Sypula <aleksandra.sypula@gmail.com>
2021-05-11 19:59:36 +0200

    Add project files.

---
+++ GameTests/GameTests.cpp
@@ -0,0 +1,15 @@
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace GameTests
+{
+	TEST_CLASS(GameTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
