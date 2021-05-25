7e7e9d7259f5f616f2749c2e4ddb7556dd6b5598
Michal <mpkowalcz@wp.pl>
2021-05-25 21:55:57 +0200

    Implemented subclass display list

--- GameApp/GameApp.cpp
+++ GameApp/GameApp.cpp
@@ -2,22 +2,72 @@
 #include "../Warriors/Warriors.h"
 #include "../Warriors/Warriors.cpp"
 #include "../Warriors/Battlefield.h"
+std::vector<Warrior*> classRepresentationsVector = {new Archer(), new Paladin(), new HolyPaladin(), new Viking()};
 
-
-template <typename T> void placeWarrior(Battlefield<T>& army, int & placementsLeft, Side s = Side::player) {
-	std::cout << "Warriors left to place " << placementsLeft << std::endl;
-	--placementsLeft;
+template <typename T> bool isSubclass(Warrior* w) {
+	if (dynamic_cast<T>(w)) {
+		return true;
+	}
+	return false;
 }
 
-template <typename T> void managePlayersTeam(Battlefield<T> & army, int maxWarriorsNumber) {
-	
-	int warriorPlacementsLeft = maxWarriorsNumber;
-	std::cout << "To put warrior in place of your choice use formatting *place_number*.*warrior_name* or *place_number*.*warrior_letter*" << std::endl << std::endl;
-	while (warriorPlacementsLeft != 0) {
-		std::cout << army.fieldForChoosing();
-		placeWarrior(army, warriorPlacementsLeft);
+
+template <typename T> void displayWarriorList() {
+	std::cout << "Warrior list" << std::endl << std::endl;
+	std::string leftAttack = "Left attacking warriors: ";
+	std::string rightAttack = "Right attacking warriors: ";
+	for (auto w : classRepresentationsVector) {
+		if (!isSubclass<T>(w))
+			continue;
+		if (w->getDirection() == -1)
+			leftAttack.append(w->getName()).append(", ");
+		if (w->getDirection() == 1)
+			rightAttack.append(w->getName()).append(", ");
 	}
-	std::cout << std::flush;
+	leftAttack.pop_back();
+	leftAttack.pop_back();
+	rightAttack.pop_back();
+	rightAttack.pop_back();
+	std::cout << leftAttack << std::endl << rightAttack << std::endl;
+}
+
+
+template <typename T> void placeWarrior(Battlefield<T>& army, Side s = Side::player) {
+	std::string userInput;
+	bool warriorChosen = false;
+	while (!warriorChosen) {
+		std::getline(std::cin, userInput);
+		if (!std::cin)
+			break;
+		if ((userInput == "List") || (userInput == "L")) {
+			displayWarriorList<T>();
+			std::cout << std::endl;
+		}
+		for (auto w : classRepresentationsVector) {
+			if (!isSubclass<T>(w))
+				continue;
+			std::string shortcut;
+			shortcut.push_back(w->identify());
+			if ((userInput == w->getName()) || (shortcut == userInput)) {
+				warriorChosen = true;
+			}
+		}
+	}
+		
+	
+}
+
+
+template <typename T> void managePlayersTeam(Battlefield<T> & army, int maxWarriorsNumber) {
+	int warriorPlacementsLeft = maxWarriorsNumber;
+	std::cout << "\"L\" or \"List\" - display warrior list\n*warrior name* or *warrior name first letter* - choose warrior\n";
+	while (warriorPlacementsLeft != 0) {
+		std::cout << std::endl << army.fieldForChoosing();
+		std::cout << "Warriors left to place: " << warriorPlacementsLeft << std::endl;
+		placeWarrior(army);
+		--warriorPlacementsLeft;
+	}
+		std::cout << std::flush;
 	system("CLS");
 }
 
@@ -25,6 +75,9 @@ template <typename T> void managePlayersTeam(Battlefield<T> & army, int maxWarri
 
 int main()
 {
+	auto w = new Paladin();
+	Warrior * j = w;
+	j->wound(1);
 	std::cout << "Placeholder Game Name" << std::endl << std::endl;
 	std::cout << "Choose puzzle" << std::endl;
 	std::cout << "1. Civillian protection" << std::endl;
